e28912d0d6a45bd8e40aa1ce2c68f382aefdbb14
giva16 <ltpie1@hotmail.com>
2024-03-06 18:31:05 +0100

    Added skeleton route codes for books router

--- router/books.js
+++ router/books.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {});
+
+router.get('/:id', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
 module.exports = router;
