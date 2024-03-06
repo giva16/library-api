f748cece7103722eaf9050eceda624f154b9b8db
giva16 <ltpie1@hotmail.com>
2024-03-06 18:24:37 +0100

    server is listening to a port assigned in .env

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+// import modules
 const express = require('express');
+require('dotenv').config();
+
+// get port number from .env file with dotenv
+const PORT = process.env.PORT;
+
+// intitialize express server
 const app = express();
-const PORT = 3000;
 
 app.listen(PORT, (req, res) => {
   console.log(`listening on port ${PORT}`);
